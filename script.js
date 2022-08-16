bb91773969fd745587e9815ebbe836357fe194c4
ognivushka <v.markevichhh@gmail.com>
2022-08-16 16:32:24 +0200

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnNavEl = document.querySelector(".button-mob-nav");
+const headerEl = document.querySelector("main-header");
+
+btnNavEl.addEventListener("click", function () {
+  btnNavEl.classList.toggle("nav-open");
+});
